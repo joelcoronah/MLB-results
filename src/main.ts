4229961216a166320e40d2c7cfaf2cd53c26ab19
joeltalo <jcorona@talo.cl>
2024-08-29 12:06:08 -0400

    :tada: first endpoint

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Callback, Context, Handler } from 'aws-lambda';
+import { configure } from '@codegenie/serverless-express';
+
+let server: Handler;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.init();
+
+  const expressHandler = app.getHttpAdapter().getInstance();
+
+  return configure({ app: expressHandler });
 }
-bootstrap();
+
+export const handler: Handler = async (
+  event: any,
+  context: Context,
+  callback: Callback,
+) => {
+  server = server ?? (await bootstrap());
+  return server(event, context, callback);
+};
